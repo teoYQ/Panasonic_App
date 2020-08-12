06480a0793c1fb00cc3ec38650b89d05b53b4051
jenyangg <39770775+jenyangg@users.noreply.github.com>
2020-08-12 23:28:31 +0800

    Updated for camera server

--- rpithreads.py
+++ rpithreads.py
@@ -17,15 +17,19 @@ import pprint
 import subprocess
 import shlex
 import serial
-
-name = "duckduckgoat"
+from signal import pause
+import rpiSelenium
+from datetime import datetime
+#name = "duckduckgoat"
 #incubator_name = "Carrot 2"
 new_incubator_name = "Technical Unit"
 pwd = "109db4a63b9cbdf01e3d74e3406baadwadwaedf"
 current_config = {
     "dose" : 0,
-    "lights" : "On",
+    "lights" : "Off",
     "temperature" : 25,
+    "sensor" : 0,
+    "fertiliser" : 1,
     }
 stop_threads = False
 
@@ -34,6 +38,8 @@ commands = queue.Queue()
 registration_queue = queue.Queue()
 device_queue = queue.Queue()
 registered_queue = queue.Queue()
+tech_queue = queue.Queue()
+temp_queue = queue.Queue()
 
 cred = credentials.Certificate("/home/pi/Downloads/capst0ned-firebase-adminsdk-oxj6s-7003ae62a1.json")
 firebase_admin.initialize_app(cred,{'databaseURL': 'https://capst0ned.firebaseio.com/'})
@@ -115,15 +121,27 @@ def register_new_incubator(incubator_name,new_pwd):
         user_and_pwd = submitted_pwds.get()
         for item in user_and_pwd.items():
             print(item)
-            name,pwd = item
+            uname,pwd = item
             if pwd == new_pwd:
-                airef.child(incubator_name).set("registered to " + name)
-                userref = db.reference(name)
+                print(uname)
+                airef.child(incubator_name).set("registered to " + uname)
+                userref = db.reference(uname)
                 userref.child(incubator_name).set(current_config)
-                print("New incubator registered under user " + name)
+                print("New incubator registered under user " + uname)
                 found = True
-    return name
-
+                print("final : " + uname)
+                return uname
+    
+def query_techunit(port,cmd):
+    response = None
+    techser = serial.Serial("/dev/"+port,115200,timeout=1)
+    techser.write(str.encode(cmd))
+    while response == None:        
+        techunitresponse = techser.readline().decode().rstrip()
+        if techunitresponse != None and techunitresponse != "":
+            return techunitresponse
+        
+        
 '''raw startup : worker definitions '''
 def incubator_subprocess():
     #name = register_new_incubator(new_incubator_name, pwd)
@@ -148,14 +166,22 @@ def registration_handler():
     logging.info("Registration Thread : starting")
     registration()
     logging.info("Registration Thread : finishing")
+    
+def tech_unit_handler():
+    logging.info("Registration Thread : starting")
+    tech_unit()
+    logging.info("Registration Thread : finishing")
 
 '''actual work'''
 def handle_updates():
     ##setup
-    duration = 5
+    current_day = datetime.today().date()
+    trigger = True
+    duration = 2
     attached_devices = {}
     active_devices = {}
     active_devices_data = {}
+    active_devices_temp = {}
     while True:
         ##New Connections/Active Devices
         #poll both queues for any updates
@@ -178,35 +204,71 @@ def handle_updates():
                 init_data_ref = db.reference(user_name+"/"+inc_name)
                 init_data = init_data_ref.get()
                 active_devices_data[inc_name] = init_data #initialise data for comparison
+                active_devices_temp[inc_name] = init_data["temperature"]
                 print("active devices : ",active_devices)
         else:
             print("Update thread: no new registration confirmations")
-        ##New Updates/Commands
-        for active_inc in active_devices.keys():
-            new_data_ref = db.reference(user_name+"/"+active_inc)
-            new_data = new_data_ref.get()
-            changed_keys,changed_values=compare_jsons(active_devices_data[active_inc],new_data)
-            if type(changed_values) == list:
-                for change in changed_values:
-                    commands.put((active_inc,change))
-                    active_devices_data[active_inc][change[0]] = change[1]
+        if list(active_devices.keys()) != []:
+            if trigger != False:
+                for active_inc in active_devices.keys():
+                    commands.put((active_inc,("c",active_devices[active_inc])))
+                trigger = False;
             else:
-                print("No changes observed for active plant unit " + active_inc)
+                if datetime.today().date() != current_day:
+                    for active_inc in active_devices.keys():
+                        commands.put((active_inc,("c",active_devices[active_inc])))
+        current_day = datetime.today().date()
+        ##New Updates/Commands
+        if list(active_devices.keys()) != []:
+            for active_inc in active_devices.keys():
+                print(active_inc)
+                ##do actual commands
+                new_data_ref = db.reference(user_name+"/"+active_inc)
+                flag = False
+                while flag != True:
+                    new_data = new_data_ref.get()
+                    if new_data != None:
+                        flag = True
+                    else:
+                        time.sleep(2)
+                if new_data != None:
+                    changed_keys,changed_values=compare_jsons(active_devices_data[active_inc],new_data)
+                    if type(changed_values) == list:
+                        for change in changed_values:
+                            if change == "temperature":
+#                                 temperature =
+#                                 print("temp change recognised")
+                                active_devices_data[active_inc][change[0]] = change[1]
+                                active_devices_temp[active_inc] = change[1]
+                                if min(active_devices_temp.values()) == change[1]:
+                                    temp_queue.put(change[1])
+                            else:
+                                commands.put((active_inc,change))
+                                active_devices_data[active_inc][change[0]] = change[1]
+                            
+                else:
+                    print("No changes observed for active plant unit " + active_inc)
         time.sleep(duration)
         
 def usb_detection():
     instructions = {
-        "dose" : {0:0,1:1},
-        "lights" : {"On":"R","Off":"r"},
+        "dose" : {0:"r",1:"R"},
+        "lights" : {"On":"W","Off":"w"},
+        "sensor" : {0:0,1:1},
+        "fertiliser" : {0:0,1:1},
+        "growth" : {datetime.today().date():0},
+        "temperature" : 2,
         }
     plant_units = {}
     usb_conns = {}
+    tech_unit = None
     cmd = "./dmsgts.sh 5"
     scanusb = "./scanusb.sh"
     old_results= [""]
     #first run, scan current devices
     print("USB Handler : First scan")
     currently_connected = subprocess.run(scanusb,stdout=subprocess.PIPE,shell=True).stdout.decode('utf-8').split("\n")
+    #currently_connected = subprocess.run(scanusb,shell=True)
     for currently_connected_device in currently_connected:
         print(currently_connected_device)
         if "USB2.0-Serial" in currently_connected_device:
@@ -228,9 +290,29 @@ def usb_detection():
                     device_queue.put((name,"added"))
                     registration_queue.put((name,pwd))
                     namequery = True
+        elif "Arduino" in currently_connected_device:
+            location = currently_connected_device.split(" - ")[0].split("/")[-1]
+            print("attempting to query /dev/" + location + " for tech unit")
+            technamequery = False
+            techser = serial.Serial("/dev/"+location,115200, timeout =1)
+            time.sleep(10)
+            techser.write(str.encode("i"))
+            while technamequery == False:
+                 techunitname = techser.readline().decode().rstrip()
+                 print(techunitname)
+                 if techunitname != "":
+                     print("TECH UNIT FOUND: NAME IS " + techunitname)
+                     #plant_units[techunitname] = location
+                     tech_unit = {"name" : techunitname, "location" : location}
+                     tech_queue.put((techunitname,location))
+                     usb_conns[location] = techunitname
+                     technamequery = True
+                 else:
+                     print("still waiting for tech unit at location " + location + " ...")
     while True:
         print("USB Handler: checking USB devices")
         result = subprocess.run(cmd,stdout=subprocess.PIPE,shell=True).stdout.decode('utf-8').split("\n")
+        #result = subprocess.run(cmd,shell=True)
         for thing in result:
             if thing not in old_results:
                  if "ch341-uart converter" in thing:
@@ -268,15 +350,85 @@ def usb_detection():
         if commands.empty() != True:
             item = commands.get()
             print("Got new command, ", item)
-            related_port = plant_units[item[0]]
             instruction = item[1]
-            command = instructions[instruction[0]][instruction[1]]
-            send_command(related_port,command)
-            commands.task_done()
+            if instruction[0] == "temperature":
+                if tech_unit != None:
+                    #print("send_command(tech_unit[location],idk what to put yet")
+                    temp_queue.put(temperature)
+                    related_port = plant_units[item[0]]
+                    print(item[1][1],type(item[1][1]))
+                    if type(item[1][1]) != dict:
+                        command = instructions[instruction[0]][instruction[1]]
+                        send_command(related_port,command)
+                        commands.task_done()
+                    else:
+                        commands.task_done()
+                else:
+                    print("tech unit not conected!")
+            elif instruction[0] == "c":
+                print("creating prediction for " + item[0])
+                related_port = plant_units[item[0]]
+                username = instruction[1]
+                raw,percentage = rpiSelenium.predict_surface_area(username,item[0],related_port)
+                db.reference(username).child(item[0]).child("growth").child(str(datetime.today().date())).set(percentage)                
+            else:
+                related_port = plant_units[item[0]]
+                print(item[1][1],type(item[1][1]))
+                if type(item[1][1]) != dict:
+                    command = instructions[instruction[0]][instruction[1]]
+                    send_command(related_port,command)
+                    commands.task_done()
+                else:
+                    commands.task_done()
         else:
             print("USB Handler : No new commands at the time")
         time.sleep(3)
-
+        
+def tech_unit():
+    tech_unit = None
+    lowest_temp = None
+    ser = None
+    while True:
+        #check if there is an update to temp
+        while temp_queue.empty() != True:
+            new_lowest_temp = temp_queue.get()
+            if lowest_temp > new_lowest_temp:
+                lowest_temp = new_lowest_temp
+            else:
+                print("logic problem for temp, but is ok second check caught it")
+        #get tech unit from usbhandler
+        while tech_queue.empty() != True:
+            tech_unit = tech_queue.get()
+            (tech_unit_name,tech_unit_location) = tech_unit
+            print("New technical unit registered : ", tech_unit)
+            ser = serial.Serial("/dev/"+tech_unit_location,115200, timeout =1)
+        if tech_unit != None:
+            print(tech_unit)
+            print("attempting to query tech unit " + tech_unit[0] + " conditions at " + tech_unit[1])
+            status = query_techunit(tech_unit_location,"2")
+            print(status)
+            temperature,ec,water_level = status.split(";")
+            cold_enough = True
+            cooling_down = False
+            if lowest_temp != None:
+                #compare temps
+                if temperature > lowest_temp:
+                    print("too hot, " + temperature + " > " + lowest_temp)
+                    cold_enough = False
+                elif temperature + 2 < lowest_temp:
+                    ser.write('0'.encode('utf-8'))
+                    print("cold enough, " + temperature + " < (by 2) " + lowest_temp)
+                    cold_enough = True
+                if cold_enough == False and cooling_down == False:
+                    ser.write('1'.encode('utf-8'))
+                    cooling_down = True
+                elif cold_enough == True and cooling_down == False:
+                    ser.write('0'.encode('utf-8'))
+                    cooling_down = False                
+        else:
+            print("No available tech unit...")
+        time.sleep(5)
+            
 def registration():
     # cred = credentials.Certificate("/home/pi/Downloads/capst0ned-firebase-adminsdk-oxj6s-7003ae62a1.json")
     # firebase_admin.initialize_app(cred,{'databaseURL': 'https://capst0ned.firebaseio.com/'})
@@ -286,21 +438,27 @@ def registration():
             incubator_name,new_pwd = item
             print("checking if " + incubator_name + " is registered")
             incref = db.reference("Active Incubators/"+incubator_name)
-            activeinc = incref.get()
-            print("ACTIVE INC",activeinc)
-            if activeinc != None:
-                if "registered" not in activeinc:
-                    print(incubator_name + " still pending registration...")
+            registered = False
+            while registered == False:
+                activeinc = incref.get()
+                print("ACTIVE INC",activeinc)
+                if activeinc != None:
+                    if "registered" not in activeinc:
+                        print(incubator_name + " still pending registration...")
+                    else:
+                        print(incubator_name + " already " + activeinc)
+                        user_name = " ".join(activeinc.split(" ")[2:])
+                        registered_queue.put((incubator_name,user_name))
+                        registration_queue.task_done()
+                        registered = True
                 else:
-                    print(incubator_name + " already " + activeinc)
-                    user_name = " ".join(activeinc.split(" ")[2:])
-                    registered_queue.put((incubator_name,user_name))
-            else:
-                print("registering new incubator " + incubator_name)
-                new_registered_user_name = register_new_incubator(incubator_name,new_pwd)
-                print(incubator_name + " successfully registered under " + new_registered_user_name)
-                registered_queue.put((incubator_name,new_registered_user_name))
-
+                    print("registering new incubator " + incubator_name)
+                    new_registered_user_name = register_new_incubator(incubator_name,new_pwd)
+                    print(incubator_name + " successfully registered under " + new_registered_user_name)
+                    registered_queue.put((incubator_name,new_registered_user_name))
+                    registration_queue.task_done()
+                    registered = True
+            
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
@@ -309,31 +467,38 @@ if __name__ == "__main__":
     # x = threading.Thread(target=thread_function, args=(1,),daemon=True)
     # logging.info("Main    : before running thread")
     # x.start()
-    x = threading.Thread(target=updates_handler,daemon=True)
-    logging.info("Main    : before running update thread")
-    x.start()
-    usbhandler = threading.Thread(target=usb_handler,daemon=True)
-    logging.info("Main    : before running usb thread")
-    usbhandler.start()
-    registrationhandler = threading.Thread(target=registration_handler,daemon=True)
-    logging.info("Main    : before running registration thread")
-    registrationhandler.start()
+    try:
+        x = threading.Thread(target=updates_handler,daemon=True)
+        logging.info("Main    : before running update thread")
+        x.start()
+        usbhandler = threading.Thread(target=usb_handler,daemon=True)
+        logging.info("Main    : before running usb thread")
+        usbhandler.start()
+        registrationhandler = threading.Thread(target=registration_handler,daemon=True)
+        logging.info("Main    : before running registration thread")
+        registrationhandler.start()
+        techunithandler = threading.Thread(target=tech_unit_handler,daemon=True)
+        logging.info("Main    : before running tech unit thread")
+        techunithandler.start()
+        pause()
     # while True:
     #     if q.empty() != True:
     #         item = q.get()
     #         print("Got item from queue, ", item)
     #         q.task_done()
-    try:
-        while True:
-            if q.empty() != True:
-                item = q.get()
-                print("Got item from queue, ", item)
-                commands.put(item)
-                q.task_done()
+    #try:
+    #    while True:
+    #        if q.empty() != True:
+    #            item = q.get()
+    #            print("Got item from queue, ", item)
+    #            commands.put(item)
+    #            q.task_done()
     except (KeyboardInterrupt,SystemExit):
         print("Stopping main process;killing threads...")
         stop_threads = True
         x.join()
+        usbhandler.join()
+        registrationahandler.join()
         logging.info("Main    : all done")
         sys.exit()
         
@@ -342,3 +507,4 @@ if __name__ == "__main__":
 # var database = firebase.database();
 # mainref = db.reference(username)
 # print(ref.get())
+
